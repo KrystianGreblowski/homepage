b8ffc227f4ddb112c294775d19f30d9ee66c9f55
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-01-16 22:37:28 +0100

    Code refactoring

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-let buttonElement = document.querySelector(".js-button");
-let bodyElement = document.querySelector(".js-body");
-let themeNameElement = document.querySelector(".js-themeName");
+{
+    const toggleBackground = () => {
+        const bodyElement = document.querySelector(".js-body");
+        const themeNameElement = document.querySelector(".js-themeName");
 
-buttonElement.addEventListener("click", () => {
-    bodyElement.classList.toggle("body--special");
-    themeNameElement.innerText = bodyElement.classList.contains("body--special") ? "jasne" : "ciemne";
-})
+        bodyElement.classList.toggle("body--special");
+        themeNameElement.innerText = bodyElement.classList.contains("body--special") ? "jasne" : "ciemne";
+    }
+
+    const init = () => {
+        const buttonElement = document.querySelector(".js-button");
+        buttonElement.addEventListener("click", toggleBackground);
+    }
+
+    init();
+}
