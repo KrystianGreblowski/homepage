c6dfea1cdbe4ee884b01b4f36f6cf1195e90a9b0
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-01-17 18:52:57 +0100

    code refactoring

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 {
-    const toggleBackground = () => {
+    const changeBackgroundColor = () => {
         const bodyElement = document.querySelector(".js-body");
-        const themeNameElement = document.querySelector(".js-themeName");
-
         bodyElement.classList.toggle("body--special");
+    }
+
+    const changeThemeName = () => {
+        const themeNameElement = document.querySelector(".js-themeName");
         themeNameElement.innerText = bodyElement.classList.contains("body--special") ? "jasne" : "ciemne";
     }
 
+    const toggleBackground = () => {
+        changeBackgroundColor();
+        changeThemeName();
+    }
     const init = () => {
         const buttonElement = document.querySelector(".js-button");
         buttonElement.addEventListener("click", toggleBackground);
