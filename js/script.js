dc48f7486b7395acd2dbcc37f505df4ac7a23738
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-01-28 16:42:18 +0100

    theme change error solved

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 {
-    const changeBackgroundColor = () => {
-        const bodyElement = document.querySelector(".js-body");
-        bodyElement.classList.toggle("body--special");
-    }
+  const changeBackgroundColor = () => {
+    const bodyElement = document.querySelector(".js-body");
 
-    const changeThemeName = () => {
-        const themeNameElement = document.querySelector(".js-themeName");
-        themeNameElement.innerText = bodyElement.classList.contains("body--special") ? "jasne" : "ciemne";
-    }
+    bodyElement.classList.toggle("body--special");
+  };
 
-    const toggleBackground = () => {
-        changeBackgroundColor();
-        changeThemeName();
-    }
-    const init = () => {
-        const buttonElement = document.querySelector(".js-button");
-        buttonElement.addEventListener("click", toggleBackground);
-    }
+  const changeThemeName = () => {
+    const bodyElement = document.querySelector(".js-body");
+    const themeNameElement = document.querySelector(".js-themeName");
 
-    init();
+    themeNameElement.innerText = bodyElement.classList.contains("body--special")
+      ? "jasne"
+      : "ciemne";
+  };
+
+  const toggleBackground = () => {
+    changeBackgroundColor();
+    changeThemeName();
+  };
+
+  const init = () => {
+    const buttonElement = document.querySelector(".js-button");
+    buttonElement.addEventListener("click", toggleBackground);
+  };
+
+  init();
 }
